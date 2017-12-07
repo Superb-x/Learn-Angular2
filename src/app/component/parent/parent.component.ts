8990c1f0157058e3653d27f612190240ed35b587
liuxianglin <nearwind_x@163.com>
2017-12-07 22:49:41 +0800

    test collepse

--- src/app/component/parent/parent.component.ts
+++ src/app/component/parent/parent.component.ts
@@ -13,25 +13,33 @@ export class ParentComponent implements OnInit {
             id: 1,
             name: '亚索',
             power: '死亡如风，常伴吾身',
+            open: true
         },
         {
             id: 2,
             name: '劫',
             power: '无形之刃，最为致命',
+            open: true
         },
         {
             id: 3,
             name: '提莫',
             power: '我去前面探探路',
+            open: true
         },
         {
             id: 4,
             name: '伊泽瑞尔',
             power: '是时候表演真正的技术了',
+            open: true
         }
     ];
     master: String = 'Master';
-    ngOnInit() { }
+    ngOnInit() {
+        for(let hero of this.heroes) {
+            hero.open = false;
+        }   
+    }
     msg = '父组件';
     heroname = '';
     names = ['亚索', '劫', '  ', '提莫'];
@@ -54,5 +62,8 @@ export class ParentComponent implements OnInit {
     onVoted(agreed: boolean) {
         console.log(agreed);
         agreed ? this.agreed++ : this.disagreed++;
+    }
+    hide (hero){    
+        hero.open = !hero.open;
     }
 }
